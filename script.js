45994c7f8f6665fd420a67e924bd616fe4256d40
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-07 19:13:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function stringChop(str, size) {
   // your code here
+	if(str.length <= size){
+		return str.split("");
+	}
+	for (let i = 0; i < str.length; i += size) {
+    let world = "";
+
+    for (let j = i; j < i + size && j < str.length; j++) {
+        world += str[j];
+    }
+
+    arr[cnt] = world;
+    cnt++;
+} 
+	
 }
 
 // Do not change the code below
